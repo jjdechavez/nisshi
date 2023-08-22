eadc79e43caf812960535eecea7a066a08dd61d3
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-08-22 09:08:09 +0800

    Populate role model hasMany users

--- app/Models/Role.ts
+++ app/Models/Role.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
 
 export default class Role extends BaseModel {
   @column({ isPrimary: true })
@@ -13,4 +14,7 @@ export default class Role extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => User)
+  public users: HasMany<typeof User>
 }
